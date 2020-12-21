b49c5de2ba7f30303cb8520b02461dbcae0b9356
Davejs136 <davejs136@gmail.com>
2020-12-21 12:52:25 -0400

    fix: demo
    refactor: rename script src
    
    build(snowpack): refactor src mount
    
    refactor: add metadata in package.json
    
    fix: use correct baseUrl in different mode (dev|prod)

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { BrowserRouter as Switch, Route } from 'react-router-dom'
 
 import Register from './views/Register'
-import Login from './views/Login';
+import Login from './views/Login'
 import Prices from './views/Prices'
 import Account from './views/Account'
-import Success from './components/Success';
+import Success from './components/Success'
+
+const baseUrl = import.meta.env.MODE === 'production' ? '/eduzonestore/' : '/'
 
 const App = () => (
-  <Switch>
+  <Switch basename={baseUrl}>
     <Route exact path="/">
       <Login />
     </Route>
