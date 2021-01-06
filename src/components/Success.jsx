f47af5817f010e0dfa45d9265640cbcc7e8d6373
robertonovanet <rhernandez@novanet.studio>
2021-01-06 11:27:02 -0430

    Ajustes de Success

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,18 +1,11 @@
-import React from 'react'
-import { withRouter } from 'react-router-dom'
-import './Success.scss'
+import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 function Success({ location }) {
   console.log(location)
 
   return (
-    <section className="success">
-      <div className="container">
-        <div className="success__info">
-          <h2 className="success__title">Success!</h2>
-        </div>
-      </div>
-    </section>
+    <p>Success!</p>
   )
 }
 
