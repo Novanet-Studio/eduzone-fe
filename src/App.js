0076f5763a0b48f157092afb3c9fc2533812451e
David Arenas <davejs136@gmail.com>
2021-01-09 23:20:20 -0400

    add: switch router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Main from './views/Main'
 import Register from './views/Register'
@@ -9,31 +9,33 @@ import Success from './components/Success'
 
 import './App.scss'
 
-const baseUrl = process.env.NODE_ENV === 'production' ? '/eduzonestore/' : '/'
+const baseUrl = process.env.NODE_ENV === 'production' ? '/eduzonestore' : '/'
 
-console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV)
 
 const App = () => (
-  <Switch basename={baseUrl}>
-    <Route exact path="/">
-      <Login />
-    </Route>
-    <Route path="/main">
-      <Main />
-    </Route>
-    <Route path="/register">
-      <Register />
-    </Route>
-    <Route path="/prices">
-      <Prices />
-    </Route>
-    <Route path="/account">
-      <Account />
-    </Route>
-    <Route path="/success">
-      <Success />
-    </Route>
-  </Switch>
+  <Router basename={baseUrl}>
+    <Switch>
+      <Route exact path="/">
+        <Login />
+      </Route>
+      <Route exact path="/main">
+        <Main />
+      </Route>
+      <Route exact path="/register">
+        <Register />
+      </Route>
+      <Route exact path="/prices">
+        <Prices />
+      </Route>
+      <Route exact path="/account">
+        <Account />
+      </Route>
+      <Route exact path="/success">
+        <Success />
+      </Route>
+    </Switch>
+  </Router>
 )
 
 export default App
