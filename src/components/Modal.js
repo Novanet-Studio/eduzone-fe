2d7edc69cedc0534ad93befdfe978c6db90a79b2
David Arenas <davejs136@gmail.com>
2021-01-15 11:47:45 -0400

    fix: modal button location

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,7 +4,8 @@ const Modal = ({ isOpen, closeModal }) => {
   const handleModalDialogClick = (e) => e.stopPropagation()
 
   return (
-    <section className={`modal ${isOpen && 'modal-open'}`} onClick={closeModal}>
+    // <section className={`modal ${isOpen && 'modal-open'}`} onClick={closeModal}>
+    <section className={`modal ${isOpen && 'modal-open'}`}>
       <div className="modal__dialog" onClick={handleModalDialogClick}>
       <button className="modal__btn-close" onClick={closeModal}>&times;</button>
         <h2 className="modal__title">Thanks for you subscription!</h2>
@@ -14,7 +15,8 @@ const Modal = ({ isOpen, closeModal }) => {
         </p>
         <div className="modal__info">
           <p className="modal__text">Access now!</p>
-          <a href="https://eduzoneserver.com/studentportal/">Click here</a>
+          <button onClick={closeModal}>access account</button>
+          <a href="https://www.eduzoneserver.com/studentportal/">Eduzone portal</a>
         </div>
       </div>
     </section>
