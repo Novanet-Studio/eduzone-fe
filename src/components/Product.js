076aeac2cc4c21de033e5142fa27360c46bc5063
RobertH-O <robertohernandez2610@gmail.com>
2021-02-17 19:23:10 -0400

    Aplicación de nuevos estilos de Inicio

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,26 +1,44 @@
 import './Product.scss'
+import ProductImg from '../assets/images/math-books-eduzone.png'
 
 const Product = ({ product, currentProductSelected, handleClick }) => (
-  <div className="product">
-    <h4 className="product__name">{product.name}</h4>
-    <p className="product__prices">{product.price}</p>
-    <div className="product__interval">Per {product.interval}</div>
-    <div className="product__billed">Billed {product.billed}</div>
+  <div className="products__card">
+    <div className="products__head">
+      <h4 className="products__title">{product.name}</h4>
+    </div>
+    <div className="products__data">
+      <p className="products__data-name">{product.name}</p>
+      <p className="products__data-description">Grades 1 to 5</p>
+      <p className="products__data-description">Access up to 5 users</p>
+      <img
+        className="products__data-img"
+        src={ProductImg}
+        alt="ProductImg"
+      />
+      <p className="products__data-price">
+        {product.price} / {product.interval} / {product.billed}
+      </p>
+    </div>
     {product.key === currentProductSelected?.key ? (
       <>
-        <input type="radio" id="producto" name="gender" defaultChecked />
-        <label htmlFor="male">{currentProductSelected.name}</label>
+        <input
+          type="radio"
+          id="producto"
+          name="gender"
+          defaultChecked
+          className="products__check"
+        />
       </>
     ) : (
       <>
         <input
           type="radio"
           id="producto"
+          className="products__check"
           onChange={() => handleClick(product.key)}
           name="gender"
           value={product.key}
         />
-        <label htmlFor="male">{product.name}</label>
       </>
     )}
   </div>
